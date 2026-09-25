607459f6a06569fc20aca4fc2c0da05b434136e0
jeanchristophe229 <christophefirst1@gmail.com>
2026-09-25 15:50:18 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -80,7 +80,7 @@ ${message}`
   status.textContent = "Ouverture de votre application email…";
 
   window.location.href =
-    `mailto:dvrkflowmusic@proton.me?subject=${subject}&body=${body}`;
+    `mailto:contact@dvrkflowmusic.spac?subject=${subject}&body=${body}`;
 });
 
 document.getElementById("year").textContent = new Date().getFullYear();
